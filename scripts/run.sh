c9757c6909752ef2c2f90ce7a662a951dbc67404
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2020-08-22 03:46:08 -0700

    [feat]: Update run script
    Details:
    Made a fancy run script that I am proud of my creative and fancy design skills ;)
    
    Resolves:
     * Local build simplified
    
    Tests:
     * Build locally and tested all the corner cases (to the best of my
     knowledge)
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,11 +4,48 @@ set -e # halt script on error
 # Delete old build
 rm -rf ./_site
 
-# Build site
-RUBYOPT='-W0' bundle exec jekyll build
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
-# Lint html output
-bundle exec htmlproofer ./_site --disable-external
+if [ ! -f ./compose.rb ];
+then
+    echo -e "${RED}Make sure you are in repo's root directory!${NC}"
+    exit
+fi
 
-# Serve on localhost
-RUBYOPT='-W0' bundle exec jekyll serve --future --drafts --incremental
+echo "Trying to build with docker..."
+
+# If docker is installed
+if ! command -v docker &> /dev/null
+then
+    echo -e "${RED}docker could not be found!${NC}\n\n"
+else
+    if ! docker stats --no-stream &> /dev/null
+    then
+        echo -e "${RED}docker deamon is not running, please start docker service${NC}\n\n"
+    else
+        docker run --rm -it -p 4000:4000 -v "$PWD:/srv/jekyll" jekyll/jekyll jekyll serve --watch --incremental --host "0.0.0.0"
+        exit
+    fi
+fi
+
+echo "Trying to build using tools installed natively..."
+
+# If bundle is installed locally
+if ! command -v bundler &> /dev/null
+then
+    echo -e "${RED}bundler could not be found${NC}\n\n"
+else
+    # Build site
+    bundle exec jekyll build
+
+    # Lint html output
+    bundle exec htmlproofer ./_site --disable-external
+
+    # Serve on localhost
+    bundle exec jekyll serve --incremental
+
+    exit
+fi
+
+echo -e "${RED}Please follow README.md for instructions${NC}"
